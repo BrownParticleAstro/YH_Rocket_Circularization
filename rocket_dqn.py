50824290dfedc1819cea404313b2361e8e8db970
Yizhong Hu <richard.hu01@gmail.com>
2022-08-08 16:08:25 -0400

    dqn test 5 tangential success

--- rocket_dqn.py
+++ rocket_dqn.py
@@ -7,7 +7,7 @@ from DQN import DeepQNetwork
 
 
 def main():
-    model = DeepQNetwork(dims=[2, 128, 128, 3],
+    model = DeepQNetwork(dims=[3, 128, 128, 3],
                          epsilon=1.0, epsilon_decay=.1, gamma=.95,
                          memory=100000, start_updating=10000,
                          batch_size=32, learning_rate=1e-4, descent_frequency=16, update_frequency=1,
@@ -28,16 +28,16 @@ def main():
     # plt.show()
 
     with rocket_gym.make('RocketCircularization-v0') as env:
-        # env = rocket_gym.PolarizeObservation(
-        #     rocket_gym.TangentialThrust(
-        #         rocket_gym.PolarizeAction(env)))
-        env = rocket_gym.RadialObservation(
-            rocket_gym.RadialThrust(
+        env = rocket_gym.PolarizeObservation(
+            rocket_gym.TangentialThrust(
                 rocket_gym.PolarizeAction(env)))
+        # env = rocket_gym.RadialObservation(
+        #     rocket_gym.RadialThrust(
+        #         rocket_gym.PolarizeAction(env)))
         # model.train(env, episodes=1000, render_frequency=1000)
         # model.save('./dqn_test_5/')
 
-        model.load('./radial_4/')
+        model.load('./dqn_test_5/')
         model.simulate(env, render=True, evaluation=True)
 
     # with rocket_gym.make('RocketCircularization-v0') as env:
