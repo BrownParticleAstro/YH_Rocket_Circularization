4b8b07e14f27715441a801e5d8ef3e9b2fedbebd
Yizhong Hu <richard.hu01@gmail.com>
2022-08-15 22:11:10 -0400

    Test for main cause of time increase

--- rocket_dqn.py
+++ rocket_dqn.py
@@ -5,27 +5,17 @@ import matplotlib.pyplot as plt
 import rocket_gym
 from DQN import DeepQNetwork
 
+from tqdm import tqdm
+from timeit import default_timer as timer
+
 
 def main():
-    model = DeepQNetwork(dims=[3, 128, 128, 3],
-                         epsilon=1.0, epsilon_decay=.1, gamma=.95,
-                         memory=100000, start_updating=10000,
-                         batch_size=32, learning_rate=1e-4, descent_frequency=16, update_frequency=1,
-                         use_target=True, target_frequency=8)
-    # model.load('./dqn_test_5/')
-    # rs = np.linspace(0.5, 1.5, 100)
-    # rdts = np.linspace(-10, 10, 100)
-    # rm, rdtm = np.meshgrid(rs, rdts)
-    # inputs = np.hstack(
-    #     (rm.reshape(-1, 1), rdtm.reshape(-1, 1)))
-    # action = tf.reshape(tf.argmax(model.q_net(inputs), axis=-1), (100, 100))
 
-    # cs = plt.contourf(rm, rdtm, action - 1, label='max Q value')
-    # plt.colorbar(cs)
-    # plt.xlabel('radius')
-    # plt.ylabel('r dot')
-    # plt.legend()
-    # plt.show()
+    model = DeepQNetwork(dims=[3, 128, 128, 3],
+                            epsilon=1.0, epsilon_decay=.1, gamma=.95,
+                            memory=100000, start_updating=50000,
+                            batch_size=32, learning_rate=1e-4, descent_frequency=800, update_frequency=8,
+                            use_target=True, target_frequency=8)
 
     with rocket_gym.make('RocketCircularization-v0') as env:
         env = rocket_gym.PolarizeObservation(
@@ -34,32 +24,16 @@ def main():
         # env = rocket_gym.RadialObservation(
         #     rocket_gym.RadialThrust(
         #         rocket_gym.PolarizeAction(env)))
-        # model.train(env, episodes=1000, render_frequency=1000)
-        # model.save('./dqn_test_5/')
+        # model.train(env, episodes=1500, render_frequency=10000, summary=True, vdo_frequency=100, vdo_path='./')
+        # model.save('./dqn_test_12/')
 
         model.load('./dqn_test_11/')
-        model.simulate(env, render=True, evaluation=True)
-
-    # with rocket_gym.make('RocketCircularization-v0') as env:
-    #     state = env.reset()
-    #     done = False
-
-    #     iters = 0
-    #     total_rwd = 0
-
-    #     while not done:
-    #         env.render()
-
-    #         new_state, reward, done, _ = env.step([0, 0])
-
-    #         state = new_state
-
-    #         iters += 1
-    #         total_rwd += reward
-
-    #     env.show()
-
-    # print(f'iters: {iters}, total_rwd: {total_rwd}')
+        # model.simulate(env, render=True, evaluation=True)
+        start = timer()
+        for _ in tqdm(range(100)):
+            model.simulate(env, render=False, evaluation=True)
+        end = timer()
+        print(f'Total Elapsed Time: {end - start}')
 
 
 if __name__ == '__main__':
