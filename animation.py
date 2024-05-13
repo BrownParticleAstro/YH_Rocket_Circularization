d4cf9c0cc24c4969b3747c25c5ad6a50b6185f79
Ben Bradley <benjamin_bradley@brown.edu>
2024-05-12 22:56:16 -0400

    test

--- animation.py
+++ animation.py
@@ -272,18 +272,20 @@ class RocketAnimation(object):
         KE = 0.5 * m * ((r_dot)**2)
         self.KEs.append(KE) # Calculate and store KE
         self.TEs.append(U+KE) # Store Total Energy
-        print(f"len(self.TEs)-- {len(self.TEs)}")
 
         dV = thrust * max_thrust * dt # dv (m/s^2) * dt (s)
         dKE = 0.5 * m * ((r_dot+dV)**2) - KE
 
-        print(f"len(self.cumm_dKEs)==0-- {len(self.cumm_dKEs)==0}")
         if len(self.cumm_dKEs)==0:
             old_cummKE = 0
         else: 
             old_cummKE = self.cumm_dKEs[-1]
         self.cumm_dKEs.append(old_cummKE+dKE)
+
         print(f"len(self.cumm_dKEs)-- {len(self.cumm_dKEs)}")
+        print(f"len(self.KEs)-- {len(self.KEs)}")
+        print(f"len(self.Us)-- {len(self.Us)}")
+        print(f"len(self.TEs)-- {len(self.TEs)}")
 
 
 if __name__ == '__main__':
