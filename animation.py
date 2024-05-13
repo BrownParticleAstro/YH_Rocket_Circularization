ed19330c8f58243d4e3823749d83b18631d03098
Ben Bradley <benjamin_bradley@brown.edu>
2024-05-13 14:50:30 -0400

    test

--- animation.py
+++ animation.py
@@ -276,7 +276,7 @@ class RocketAnimation(object):
         self.TEs.append(U+KE) # Store Total Energy
 
         dV = thrust * max_thrust * dt # dv (m/s^2) * dt (s)
-        dKE = (0.5 * m * ((r_dot+np.linalg.norm(dV))**2))-KE
+        dKE = ((0.5 * m * ((r_dot+np.linalg.norm(dV))**2))-KE) / 2
 
         if len(self.cumm_dKEs)==0:
             old_cummKE = 0
